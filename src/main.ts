250c8d63c47ef2171b6b2a602ebafba66ddfdd68
rajkoradiya <rajkoradiya003@gmail.com>
2025-01-25 13:15:23 +0530

    change port

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { bodyParser: true });
+  const port = process.env.PORT || 4000;
   app.enableCors({
     origin: process.env.CORS_API,
   });
-  await app.listen(8081);
+  await app.listen(port);
 }
 bootstrap();
