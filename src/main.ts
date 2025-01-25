a627e06cfa09b6fccc8982eff1dc984699cd883c
rajkoradiya <rajkoradiya003@gmail.com>
2025-01-25 12:55:21 +0530

    add env variable

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { bodyParser: true });
   app.enableCors({
-    origin: 'http://localhost:8080',
+    origin: process.env.CORS_API,
   });
   await app.listen(3030);
 }
